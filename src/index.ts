be04a825824606eb5f38d5739887a03b75946e90
Reinildo Reis <reinildo@nucleoti.com>
2021-06-07 14:26:27 -0300

    Exported Interface IInstance

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { ISpriteInstance } from "./construct/plugins/ISpriteInstance";
 import { IObjectClass } from "./construct/objects/IObjectClass";
 import { ITextInstance } from "./construct/plugins/ITextInstance";
 import { ISpriteFontInstance } from "./construct/plugins/ISpriteFontInstance";
+import { IInstance } from "./construct/objects/IInstance";
 
 export {
   IRuntime,
   ISpriteInstance,
   IObjectClass,
   ITextInstance,
-  ISpriteFontInstance
+  ISpriteFontInstance,
+  IInstance
 }
