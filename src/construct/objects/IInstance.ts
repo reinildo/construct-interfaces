df1d0e13b6939e2f7880427d4b134ab8ba6af27a
Reinildo Reis <reinildo@nucleoti.com>
2021-06-08 12:48:15 -0300

    addition more pros IIstance and IObject

--- src/construct/objects/IInstance.ts
+++ src/construct/objects/IInstance.ts
@@ -6,6 +6,12 @@ interface InstanceEvents {
 
 
 interface IInstance extends IObjectClass {
+
+  /**
+   * Add or remove an event handler for a particular type of event fired by an addon's script interface. For information on which events are fired by specific addons, see the documentation on each addon's script interfaces.
+   * @param type 
+   * @param callback 
+   */
   addEventListener(type:string, callback:Function):void
   removeEventListener(type:string, callback:Function):void
 
@@ -15,6 +21,12 @@ interface IInstance extends IObjectClass {
    * Destroy the instance, removing it and releasing any memory associated with it.
    */
   destroy():void
+
+  /**
+   * The IObjectClass interface for this instance's object type.
+   * This is named objectType and not objectClass because it always refers to an object type, and never a family.
+   */
+  objectType:IObjectClass
 }
 
 export { IInstance, InstanceEvents }
--- src/construct/objects/IObjectClass.ts
+++ src/construct/objects/IObjectClass.ts
@@ -3,6 +3,11 @@ import { IWordInstance } from "./IWordInstance";
 
 interface IObjectClass {
   /**
+   * A read-only string of the object class's name.
+   */
+  name:string
+  
+  /**
    * Create a new instance of the object type at a position. The layer to create on is specified either by a case-insensitive string of the layer name or its zero-based index. The position is given in layout co-ordinates. If createHierarchy is true, all children of the created instance in the scene-graph hierarchy will also be created automatically with their connections in place. Returns an instance class representing the created instance.
    * @param layerNameOrIndex - The layer name [string] or index [number] to create the instance
    * @param x - The x position of new instance
